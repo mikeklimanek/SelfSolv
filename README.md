5fcb23086e396f69d4a54b6715f20a1f51b627d1
mikeklimanek <mike.klimanek@gmail.com>
2024-01-13 22:39:03 +0100

    defined main

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SelfSolv
 1) create a window
+2) finishing window actions
+3) implement main()
+4) 
--- main.py
+++ main.py
@@ -7,5 +7,25 @@ class Window:
         self.canvas = tk.Canvas(self.root, width=width, height=height)
         self.canvas.pack()
         self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+def main():
+    window = Window(800, 600)
+    window.wait_for_close()
+
+if __name__ == "__main__":
+    main()
 
 
