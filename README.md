396d9d2c6238a9f66f91953af37b92cfa8673ea1
mikeklimanek <mike.klimanek@gmail.com>
2024-01-13 22:52:34 +0100

    points and lines

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 1) create a window
 2) finishing window actions
 3) implement main()
-4) 
+4) splitting into coresponding files
+5) creating point and lines
+6) 
--- main.py
+++ main.py
@@ -1,25 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
-
-class Window:
-    def __init__(self, width, height):
-        self.root = tk.Tk()
-        self.root.title("My Window")
-        self.canvas = tk.Canvas(self.root, width=width, height=height)
-        self.canvas.pack()
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-
-    def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-
-    def wait_for_close(self):
-        self.running = True
-        while self.running:
-            self.redraw()
-
-    def close(self):
-        self.running = False
+from window import Window
+from points import Point, Line
 
 def main():
     window = Window(800, 600)
