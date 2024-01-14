2d676c1342037bacc48e0361a2681648681a1e7e
mikeklimanek <mike.klimanek@gmail.com>
2024-01-14 20:04:31 +0100

    file spreading

--- main.py
+++ main.py
@@ -2,30 +2,7 @@
 import tkinter as tk
 from tkinter import BOTH, Canvas
 import time
-
-class Window:
- def __init__(self, width, height):
-     self.root = tk.Tk()
-     self.root.title("My Window")
-     self.canvas = tk.Canvas(self.root, width=width, height=height)
-     self.canvas.pack()
-     self.running = False
-     self.root.protocol("WM_DELETE_WINDOW", self.close)
-
- def redraw(self):
-     self.root.update_idletasks()
-     self.root.update()
-
- def wait_for_close(self):
-     self.running = True
-     while self.running:
-         self.redraw()
-
- def close(self):
-     self.running = False
-
- def draw_line(self, line, fill_color):
-     line.draw(self.canvas, fill_color)
+from window import Window
 
 class Point:
  def __init__(self, x, y):
