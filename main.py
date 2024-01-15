878d93e763ad955b5e2c662d8f04fb826af9a5e7
mikeklimanek <mike.klimanek@gmail.com>
2024-01-15 23:27:54 +0100

    centered and increased the size of the maze

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ def center_window(win):
    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
 def main():
- win = Window(800, 600)
- center_window(win)
- maze = Maze(0, 0, 5, 5, 100, 100, win)
- maze._break_entrance_and_exit()
- win.wait_for_close()
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
+    win.wait_for_close()
 
 if __name__ == "__main__":
  main()
