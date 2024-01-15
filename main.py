3155771b873c799eeb72fa0e176e2e4c88b0e457
mikeklimanek <mike.klimanek@gmail.com>
2024-01-15 23:06:04 +0100

    changes to exit and entry

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from graphics import *
 def main():
  win = Window(800, 600)
  maze = Maze(0, 0, 5, 5, 100, 100, win)
+ maze._break_entrance_and_exit()
  win.wait_for_close()
 
 if __name__ == "__main__":
