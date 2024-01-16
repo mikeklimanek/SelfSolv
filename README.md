8470af7d95f7bd8cdbe3941ffdc82cdfd2b1837a
mikeklimanek <mike.klimanek@gmail.com>
2024-01-16 23:46:18 +0100

    gif example

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # SelfSolv
+![](SelfSolv.gif)
+
 1) create a window
 2) finishing window actions
 3) implement main()
--- graphics.py
+++ graphics.py
@@ -166,7 +166,7 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.01)
+        time.sleep(0.02)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
